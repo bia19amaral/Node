d35baa278e86861ae01b1fafe68c86bbace3c3f3
bia19amaral <amaralamorimb@gmail.com>
2022-11-25 10:46:20 -0300

    Versão 4 - Placar próximo jogo do Brasil

--- aula1.js
+++ aula1.js
@@ -3,6 +3,7 @@ var http = require('http');
 var dia = require("./biblioteca");
 http.createServer(function(req,res){
     res.writeHead(200,{"Content-Type":"text/html"});
+    res.write('<h2>Brasil 3 x 0 Suíça</h2> \n');
     res.write('<h2>Brasil 2 x 0 Servia</h2> \n');
     res.write("<p style=color:pink>"+Date().substring(16,24)+"<br>");
     res.write(dia.diaDaSemana()+"</p><br>");
