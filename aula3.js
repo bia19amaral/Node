a771e6f32f078a8024312f386d0d7527ab47cb7a
bia19amaral <amaralamorimb@gmail.com>
2022-12-08 09:56:54 -0300

    Aula 3 - JS

--- aula3.js
+++ aula3.js
@@ -1,10 +1,17 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 http.createServer(function(req, res){
+    var q = url.parse(req.url, true);
+    var filename = "."+ q.pathname;
+    fs.readFile(filename, function(err,data){
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("Pagina Invalida!");
+        }
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
-    var q = url.parse(req.url, true).query;
-    var txt = '<br>dia: '+q.dia+'<br>mes: '+q.mes+'<br> ano: '+q.ano;
-    res.end(txt);
+    res.write(data);
+    return res.end();
+})    
 }).listen(80);
